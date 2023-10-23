ed579506af627f98ec9140c44c21c50673a35bba
natalygary <94802299+natalygary@users.noreply.github.com>
2023-10-23 18:40:48 -0400

    Basis of App Development
    This is the starting code for the App.

---
+++ Tired Of Measuring/Tired Of Measuring/Tired_Of_MeasuringApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tired_Of_MeasuringApp.swift
+//  Tired Of Measuring
+//
+//  Created by Nataly Garcia on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Tired_Of_MeasuringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
